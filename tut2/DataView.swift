21d58ae3f28df96dd199151fd98d296dfeb0e5b2
CHOW Chun Wang <ccwangc29@gmail.com>
2019-11-02 13:38:37 +0800

    Added table view

---
+++ tut2/DataView.swift
@@ -0,0 +1,63 @@
+//
+//  DataView.swift
+//  tut2
+//
+//  Created by brianchow on 1/11/2019.
+//  Copyright © 2019 KooKi. All rights reserved.
+//
+
+import UIKit
+
+var books = ["abc", "def", "ghi"]
+
+class DataView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    
+    let textCellIdentifier = "TextCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
+     
+        let row = indexPath.row
+        cell.textLabel?.text = books[row]
+     
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let row = indexPath.row
+        print(books[row])
+        
+        performSegue(withIdentifier: "showDetail", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tut2/DetailView.swift
@@ -0,0 +1,31 @@
+//
+//  DetailView.swift
+//  tut2
+//
+//  Created by brianchow on 1/11/2019.
+//  Copyright © 2019 KooKi. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
