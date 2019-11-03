802f450999be07f388278180e58e6e4fe633c2a3
raymond <raymondchan0105@gmail.com>
2019-11-03 15:38:16 +0800

    detailedView added table view

--- tut2/Controller/DetailView.swift
+++ tut2/Controller/DetailView.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class DetailView: UIViewController {
+class DetailView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     
     var bookTitle = " "
     var bookAuthor = " "
@@ -19,28 +21,73 @@ class DetailView: UIViewController {
     var publishDate = " "
     var numPage = " "
     
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var authorLabel: UILabel!
-    @IBOutlet var publisherLabel: UILabel!
-    @IBOutlet var isbnLabel: UILabel!
-    @IBOutlet var imageUrlLabel: UILabel!
-    @IBOutlet var bookUrlLabel: UILabel!
-    @IBOutlet var publishDateLabel: UILabel!
-    @IBOutlet var numPageLabel: UILabel!
+    //@IBOutlet var titleLabel: UILabel!
+    //@IBOutlet var authorLabel: UILabel!
+    //@IBOutlet var publisherLabel: UILabel!
+    //@IBOutlet var isbnLabel: UILabel!
+    //@IBOutlet var imageUrlLabel: UILabel!
+    //@IBOutlet var bookUrlLabel: UILabel!
+    //@IBOutlet var publishDateLabel: UILabel!
+    //@IBOutlet var numPageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = bookTitle
-        authorLabel.text = bookAuthor
-        publisherLabel.text = bookPublisher
-        isbnLabel.text = bookIsbn
-        imageUrlLabel.text = bookImageUrl
-        bookUrlLabel.text = bookUrl
-        publishDateLabel.text = publishDate
-        numPageLabel.text = numPage
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //titleLabel.text = bookTitle
+        //authorLabel.text = bookAuthor
+        //publisherLabel.text = bookPublisher
+        //isbnLabel.text = bookIsbn
+        //imageUrlLabel.text = bookImageUrl
+        //bookUrlLabel.text = bookUrl
+        //publishDateLabel.text = publishDate
+        //numPageLabel.text = numPage
     }
     
-
+    @IBOutlet var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = bookIsbn
+        }
+        else if indexPath.section == 1 {
+            cell.textLabel?.text = bookTitle
+        }
+        else if indexPath.section == 2 {
+            cell.textLabel?.text = bookAuthor
+        }
+        else if indexPath.section == 3 {
+            cell.textLabel?.text = bookPublisher
+        }
+        else if indexPath.section == 4 {
+            cell.textLabel?.text = publishDate
+        }
+        else if indexPath.section == 5 {
+            cell.textLabel?.text = numPage
+        }
+        else if indexPath.section == 6 {
+            cell.textLabel?.text = bookImageUrl
+        }
+        else if indexPath.section == 7 {
+            cell.textLabel?.text = bookUrl
+        }
+        return cell
+    }
+    
+    
     /*
     // MARK: - Navigation
 
